f7e8e849ebdea101fab6dc73ab2d20e5564df904
Tom Ecrepont <tomecrepont@gmail.com>
2021-07-18 15:51:36 +0200

    Set up of the first locale file

--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,9 @@ module ApiBillicopresto
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
+    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
+    config.i18n.default_locale = :fr
+
     # Don't generate system test files.
     config.generators.system_tests = nil
   end
