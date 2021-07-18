2c2b73e1789b7bd824593897f5b1386e94ecbb78
Tom Ecrepont <tomecrepont@gmail.com>
2021-07-18 17:25:19 +0200

    Add the open data paris client and full address to retailer

---
+++ app/lib/open_data_paris/open_data_paris_client.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require "httparty"
+
+class OpenDataParisClient
+  HOST = "https://opendata.paris.fr"
+
+  class << self
+    def fetch_grocery_store(rows)
+      url = "#{HOST}/api/records/1.0/search/?dataset=coronavirus-commercants-parisiens-livraison-a-domicile&q=&rows=#{rows}&facet=code_postal&facet=type_de_commerce&facet=fabrique_a_paris&facet=services&refine.type_de_commerce=Alimentation+g%C3%A9n%C3%A9rale+et+produits+de+premi%C3%A8re+n%C3%A9cessit%C3%A9"
+
+      response = HTTParty.get(url)
+      JSON.parse(response.body).dig("records")
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-20.times do
+number_of_devise_entity = 20
+
+
+ap I18n.t 'seed.creation_of_many', number: number_of_devise_entity, model: Retailer.name
+number_of_devise_entity.times do
+	ap I18n.t 'seed.creation_of_one', model: Retailer.name
 	fake_name = Faker::Company.name
 	Retailer.create!(name: fake_name, 
 		email: "#{fake_name.gsub(/[^0-9a-z]/i, '')}@example.com",
@@ -16,7 +21,25 @@
 		city: random_addresse_generator[:nom_commune],
 		password: "Test.123", 
 		password_confirmation: "Test.123"
-		)	
+		)
+	ap I18n.t 'seed.users.user_created', number: number_of_devise_entity, model: Retailer.name	
+end
+
+number_of_devise_entity.times do
+	first_name =  Faker::Name.first_name
+	last_name = Faker::Name.last_name
+	User.create!(
+		email: "#{first_name}.#{last_name}@example.com",
+		first_name: first_name,
+		last_name: last_name,
+		password: "Test.123", 
+		password_confirmation: "Test.123"
+		)
+
+end
+
+User.each do
+
 end
 
 private 
@@ -25,6 +48,6 @@ def random_addresse_generator
 	@random_addresse ||= addresse_data_base[Random.new.rand(1..addresse_data_base.count)].to_h
 end
 
-def addresse_data_base
-	@addresse_data_base ||= CSV.table("db/addresses_51.csv", col_sep: ';')
+def grocery_store_database
+	@grocery_store_database ||= 
 end
