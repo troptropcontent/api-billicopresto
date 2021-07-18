1b01133c8c3e745e2dda81a56393fe1321e44e50
Tom Ecrepont <tomecrepont@gmail.com>
2021-07-18 16:05:23 +0200

    Add the brand enum to the retailer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_18_125904) do
+ActiveRecord::Schema.define(version: 2021_07_18_140405) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,6 +76,7 @@ ActiveRecord::Schema.define(version: 2021_07_18_125904) do
     t.string "region"
     t.string "zip_code"
     t.string "city"
+    t.integer "brand"
     t.index ["email"], name: "index_retailers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_retailers_on_reset_password_token", unique: true
   end
