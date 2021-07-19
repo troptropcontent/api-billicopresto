083fb583174e494fa00209b981ca8e53d64dfe82
Tom Ecrepont <tomecrepont@gmail.com>
2021-07-19 23:48:11 +0200

    Work on the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,53 +1,39 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require "./app/lib/open_data_paris/open_data_paris_client"
 
+grocery_store_database ||= OpenDataParisClient.fetch_grocery_store(50)
 number_of_devise_entity = 20
 
-
+ap I18n.t 'seed.reseting_model_database', model: Retailer.name
+Retailer.delete_all
 ap I18n.t 'seed.creation_of_many', number: number_of_devise_entity, model: Retailer.name
-number_of_devise_entity.times do
+grocery_store_database.each do |grocery_store|
+	data = grocery_store["fields"]
+	next unless data["mail"]
 	ap I18n.t 'seed.creation_of_one', model: Retailer.name
-	fake_name = Faker::Company.name
-	Retailer.create!(name: fake_name, 
-		email: "#{fake_name.gsub(/[^0-9a-z]/i, '')}@example.com",
-		street: random_addresse_generator[:nom_voie], 
-		zip_code: random_addresse_generator[:code_postal],
-		number: random_addresse_generator[:numero],
-		city: random_addresse_generator[:nom_commune],
+	retailer = Retailer.create!(name: data["nom_du_commerce"], 
+		email: data["mail"],
+		full_address: data["adresse"], 
+		zip_code: data["code_postal"],
+		city: "Paris",
 		password: "Test.123", 
 		password_confirmation: "Test.123"
 		)
-	ap I18n.t 'seed.users.user_created', number: number_of_devise_entity, model: Retailer.name	
+	ap I18n.t 'seed.retailers.retailer_created', name: retailer.name, full_address: retailer.full_address, zip_code: retailer.zip_code, city: retailer.city, email: retailer.email
+	break if Retailer.count == number_of_devise_entity
 end
 
+ap I18n.t 'seed.reseting_model_database', model: User.name
+User.delete_all
+ap I18n.t 'seed.creation_of_many', number: number_of_devise_entity, model: User.name
 number_of_devise_entity.times do
 	first_name =  Faker::Name.first_name
 	last_name = Faker::Name.last_name
-	User.create!(
+	user = User.create!(
 		email: "#{first_name}.#{last_name}@example.com",
 		first_name: first_name,
 		last_name: last_name,
 		password: "Test.123", 
 		password_confirmation: "Test.123"
 		)
-
-end
-
-User.each do
-
-end
-
-private 
-
-def random_addresse_generator
-	@random_addresse ||= addresse_data_base[Random.new.rand(1..addresse_data_base.count)].to_h
-end
-
-def grocery_store_database
-	@grocery_store_database ||= 
+	ap I18n.t 'seed.users.user_created', first_name: user.first_name, last_name: user.last_name, email: user.email
 end
