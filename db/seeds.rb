15e862094caa551d720413cb1f1f634c45de13d7
Tom Ecrepont <tomecrepont@gmail.com>
2021-07-21 21:50:41 +0200

    Updated the seed to now create 20 users and 20 retailers

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,15 +25,20 @@ end
 ap I18n.t 'seed.reseting_model_database', model: User.name
 User.delete_all
 ap I18n.t 'seed.creation_of_many', number: number_of_devise_entity, model: User.name
-number_of_devise_entity.times do
+
+number_of_entity_created = 0
+while number_of_entity_created <= number_of_devise_entity do
 	first_name =  Faker::Name.first_name
 	last_name = Faker::Name.last_name
-	user = User.create!(
-		email: "#{first_name}.#{last_name}@example.com",
+	user = User.create(
+		email: I18n.transliterate("#{first_name}.#{last_name}@example.com"),
 		first_name: first_name,
 		last_name: last_name,
 		password: "Test.123", 
 		password_confirmation: "Test.123"
 		)
-	ap I18n.t 'seed.users.user_created', first_name: user.first_name, last_name: user.last_name, email: user.email
+	next unless user.valid?
+	ap I18n.t 'seed.users.user_created', first_name: user.first_name, last_name: user.last_name, email: user.email 
+	number_of_entity_created += 1
 end
+
