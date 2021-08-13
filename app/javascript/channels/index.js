14bcb460deac65800f2473baf15cef7c32ea5798
agathesf <agathe.saintfrison@gmail.com>
2021-08-14 00:38:08 +0200

    test anim views js : fail

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -1,16 +1,2 @@
 // Load all the channels within this directory and all subdirectories.
 // Channel files must be named *_channel.js.
-
-const channels = require.context('.', true, /_channel\.js$/)
-channels.keys().forEach(channels)
-
-
-document.body.addEventListener('click', function(e){
-    var target = e.target;
-    if(target.classList.contains('anim')) {
-        setTimeout(function() {
-            if(target.dataset) window.location = target.dataset.redirect;
-            else window.location = target.getAttribute('href');
-            }, 1000);
-    }
-}, false);
