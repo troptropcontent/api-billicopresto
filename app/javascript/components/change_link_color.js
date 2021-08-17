d3e45f7ef4d9fa869f3159266af2e5aea5440b0a
agathesf <agathe.saintfrison@gmail.com>
2021-08-17 13:14:05 +0200

    js : working / show : done

--- app/javascript/components/change_link_color.js
+++ app/javascript/components/change_link_color.js
@@ -1,45 +1,31 @@
 function changeLinkColor() {
 
 	var currentUrl = window.location.href;
+	console.log(currentUrl.toString());
 
-	var profileUrl = document.getElementById("profile").href;
-	var profile = document.getElementById("profile");
+	// var profileUrl = document.getElementById("profile").href;
+	var profile = document.getElementById('dashboard');
 
-	var budgetUrl = document.getElementById("budget").href;
+	// var budgetUrl = document.getElementById("budget").href;
 	var budget = document.getElementById("budget");
 
-	var receiptsUrl = document.getElementById("receipts").href;
+	// var receiptsUrl = document.getElementById("receipts").href;
 	var receipts = document.getElementById("receipts");
 
-	var scanUrl = document.getElementById("scan").href;
+	// var scanUrl = document.getElementById("scan").href;
 	var scan = document.getElementById("scan");
 
-	var promoUrl = document.getElementById("promo").href;
+	// var promoUrl = document.getElementById("promo").href;
 	var promo = document.getElementById("promo");
 
-	var warrantiesUrl = document.getElementById("warranties").href;
+	// var warrantiesUrl = document.getElementById("warranties").href;
 	var warranties = document.getElementById("warranties");
 
-	if (currentUrl === profileUrl) {
-		profile.classList.add("active");
-	}
-	else if (currentUrl === budgetUrl) {
-		budget.classList.add("active");
-	}
-	else if (currentUrl === receiptsUrl) {
+	if (currentUrl.includes("/receipts")) {
 		receipts.classList.add("active");
 	}
-	else if (currentUrl === scanUrl) {
-		scan.classList.add("active");
-	}
-	else if (currentUrl === promoUrl) {
-		promo.classList.add("active");
-	}
-	else if (currentUrl === warrantiesUrl) {
-		warranties.classList.add("active");
-	}
 	else {
-		console.log("no active link")
+		profile.classList.add("active");
 	}
 }
 
