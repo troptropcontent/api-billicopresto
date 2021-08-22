7a6eb244900050686dadc5d59227c1f289b0fa28
Tom Ecrepont <tomecrepont@gmail.com>
2021-08-22 22:06:16 +0200

    Creation of the mapper service who add cents and merge the operator with there field

--- app/services/controllers/filter_params_mapper_service.rb
+++ app/services/controllers/filter_params_mapper_service.rb
@@ -1,22 +1,62 @@
 class FilterParamsMapperService
 
-  SUFFIX_REQUIRING_TRANSFORMATION = ["_cent"] 
+  MONEY_FIELDS = %w[
+    unit_price
+    amount_excluding_taxes
+    amount_including_taxes
+    amount_taxes
+  ]
+
+  SUFFIX_OPERATOR = %w[
+    _less
+    _more
+    _not]
     
-
-
-  def initialize(filter_params)
+  def initialize(filters, filter_params)
+    @filters = filters
     @filter_params = filter_params
-    @mapped_params = filter_params
   end
 
   def call!
-
+    add_operators
+    map_filters
+    @filters
   end
 
   private
 
-  def mapped_value(key, value)
-    return @mapped_params[key] unless @mapped_params[value].end_with?(*SUFFIX_REQUIRING_TRANSFORMATION)
-    @mapped_params[key]*100
+  def map_filters
+    convert_cents
   end
+
+  def convert_cents
+
+    regex = "^(#{MONEY_FIELDS.join('|')})+(#{SUFFIX_OPERATOR.join('|')}|)$"
+
+    money_fields = @filters.select{|key, value| key.match(/#{regex}/) }
+
+    money_fields.each do |key,value|
+      @filters[key_cents(key)] = value.to_f*100.to_i
+      @filters.delete(key)
+    end
+
+    @filters
+  end
+
+  def key_cents(key)
+    regex = "(#{SUFFIX_OPERATOR.join('|')})$"
+    suffix = key.match(regex)&.to_s
+    suffix ? key.gsub(/#{regex}/, "_cent#{suffix}") : "#{key}_cent"
+  end
+
+  def add_operators
+    filter_operators = @filter_params.select{|k,v| k.end_with?("_operator") }
+    filter_operators.each do |filter_operator, value|
+      field = filter_operator.delete_suffix("_operator")
+      next unless @filters[field]
+      @filters["#{field}_#{value}"] = @filters[field]
+      @filters.delete(field)
+    end
+  end
+
 end
