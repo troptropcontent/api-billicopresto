784448ef98d0d09f0e11c35dc73984b71623e3cc
agathesf <agathe.saintfrison@gmail.com>
2021-08-24 16:39:41 +0200

    add first name and last name to registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,15 @@
 class ApplicationController < ActionController::Base
+	  before_action :configure_permitted_parameters, if: :devise_controller?
 
 	rescue_from CanCan::AccessDenied do
 	  render json: {code: :forbidden, message: I18n.t("cancan.access_denied")}, status: :forbidden
+	end
+
+	protected
+
+	def configure_permitted_parameters
+	  devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+	  devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
 	end
 
 end
