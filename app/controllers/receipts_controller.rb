0f888656910ca3d0c10fc4e4a4f4a20cf78fbd75
Tom Ecrepont <tomecrepont@gmail.com>
2021-09-01 23:37:23 +0200

    avoid & to be called on parameters (#6)
    * add safe operator to avoid failure
    
    * Update the controller to avoid & to be called on parameters (#5)
    
    * fix & called on parameters
    
    * add controllers:: to avoid issue with file autoloading

--- app/controllers/receipts_controller.rb
+++ app/controllers/receipts_controller.rb
@@ -10,9 +10,9 @@ class ReceiptsController < ApplicationController
   ]
 
   def index
-    @receipts = @receipts.includes(:till) if filter_params&["till.retailer"]&.compact_blank.presence
-    @receipts = @receipts.includes(till: :retailer) if filter_params&["retailer.city"]
-    @receipts = FilterService.new(@receipts,FIELD_FILTER_WHITELIST, filter_params).filter! if filter_params
+    @receipts = @receipts.includes(:till) if filter_params && filter_params["till.retailer"]&.compact_blank.presence
+    @receipts = @receipts.includes(till: :retailer) if filter_params && filter_params["retailer.city"]
+    @receipts = Controllers::FilterService.new(@receipts,FIELD_FILTER_WHITELIST, filter_params).filter! if filter_params
     @receipts_ordered = @receipts.group_by { |t| t.created_at.beginning_of_year } 
     @receipts_ordered_month = @receipts.group_by { |t| t.created_at.beginning_of_month } 
   end
