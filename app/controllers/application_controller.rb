8fd1c0c2b8a4456622ce9c84e00316202ee44090
Tom Ecrepont <tomecrepont@gmail.com>
2021-09-05 18:04:38 +0200

    MEP 05/09 (#8)
    * add safe operator to avoid failure
    
    * Update the controller to avoid & to be called on parameters (#5)
    
    * fix & called on parameters
    
    * add controllers:: to avoid issue with file autoloading
    
    * Front end filter (#7)
    
    * Add first name and last name to devise
    
    * small front update on index and show
    
    * needed to updates some small features
    
    * filter front : done (90%)
    
    * filter css 100% done
    
    Co-authored-by: agathesf <76791952+agathesf@users.noreply.github.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,16 @@
 class ApplicationController < ActionController::Base
 	require './app/services/controllers/filter_service.rb'
+	before_action :configure_permitted_parameters, if: :devise_controller?
 
 	rescue_from CanCan::AccessDenied do
 	  render json: {code: :forbidden, message: I18n.t("cancan.access_denied")}, status: :forbidden
+	end
+
+	protected
+
+	def configure_permitted_parameters
+	  devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+	  devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
 	end
 
 end
