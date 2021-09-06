0ced827747a9b6b393356cab9bbbaee7ca1088f4
Tom Ecrepont <tomecrepont@gmail.com>
2021-09-06 22:14:42 +0200

    add start and end date to voucher

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_06_200539) do
+ActiveRecord::Schema.define(version: 2021_09_06_201030) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -128,6 +128,8 @@ ActiveRecord::Schema.define(version: 2021_09_06_200539) do
     t.integer "kind"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.date "start_date"
+    t.date "end_date"
     t.index ["retailer_id"], name: "index_vouchers_on_retailer_id"
   end
 
