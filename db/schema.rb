8e50f74b4b58017df9c887930b4eedfd32796024
Tom Ecrepont <tomecrepont@gmail.com>
2021-09-06 22:33:54 +0200

    add the barcode column to product

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_06_201030) do
+ActiveRecord::Schema.define(version: 2021_09_06_202838) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2021_09_06_201030) do
     t.integer "unit"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "barcode"
   end
 
   create_table "receipt_lines", force: :cascade do |t|
