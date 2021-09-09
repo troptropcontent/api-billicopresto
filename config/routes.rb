77f42fad7b8a87074fdc12e598adc1755b1889b0
agathesf <agathe.saintfrison@gmail.com>
2021-09-09 21:21:45 +0200

    update landing page

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # LANDING-PAGE
-  get 'pages/home' , to: 'pages#home', as: 'welcome'
+  get 'welcome' , to: 'pages#home', as: 'welcome'
 
 end
