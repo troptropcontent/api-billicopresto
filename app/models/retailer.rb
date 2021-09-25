8f42147e822317b387edba2b3c3e21ede9ec26f4
Tom Ecrepont <tomecrepont@gmail.com>
2021-09-25 17:23:45 +0200

    MEP 25/09 (#25)
    * set up the db:import_production_dump to import the last dump and retore the data in local env (#24)
    
    * Create voucher seed (#23)

--- app/models/retailer.rb
+++ app/models/retailer.rb
@@ -5,13 +5,50 @@ class Retailer < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :tills
+  has_many :receipts, through: :tills
   has_many :items
+  has_many :products, through: :items
   has_many :vouchers
 
   enum brand: [:carrefour_market, :monoprix, :auchan]
   
+  def bigest_consumer_by_product(product, n = nil)
+    users = User.joins(receipts: [:till, receipt_lines: {item: :product}])
+                            .where(tills: {retailer: self}, 'item.product_id': product.id)
+                            .group(:id, :product_id)
+                            .order('sum_quantity DESC')
+                            .sum(:quantity).to_a
+    
+    users = users.last(n) if n
 
-  def receipts
-    Receipt.includes(:till).where(tills:{retailer: self})
+    bigest_consumer_by_product_mapper(users)
+  end
+
+  def sales_by_product
+    products = self.products.joins(items: {receipt_lines: {receipt: :till}})
+        .where(tills: {retailer: self})
+        .group(:id)
+        .order('sum_quantity DESC')
+        .sum(:quantity).to_a
+
+    sales_by_product_mapper(products)
+  end
+
+  private
+
+  def sales_by_product_mapper(products)
+    products.map do |product|
+      keys = [:product_id, :quantity]
+      values = product.flatten
+      [keys, values].transpose.to_h
+    end
+  end
+
+  def bigest_consumer_by_product_mapper(users)
+    users.map do |user|
+      keys = [:user_id, :product_id, :quantity]
+      values = user.flatten
+      [keys,values].transpose.to_h
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_08_195626) do
+ActiveRecord::Schema.define(version: 2021_09_24_185607) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -132,6 +132,8 @@ ActiveRecord::Schema.define(version: 2021_09_08_195626) do
     t.date "start_date"
     t.date "end_date"
     t.bigint "item_id", null: false
+    t.string "target_query"
+    t.integer "discount_cents", default: 0
     t.index ["item_id"], name: "index_vouchers_on_item_id"
     t.index ["retailer_id"], name: "index_vouchers_on_retailer_id"
   end
