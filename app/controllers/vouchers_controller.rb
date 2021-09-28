4a7bda55dae89b95c66ab31e6fa008b61ac1f409
agathesf <agathe.saintfrison@gmail.com>
2021-09-28 10:38:58 +0200

    modif front after feeback

--- app/controllers/vouchers_controller.rb
+++ app/controllers/vouchers_controller.rb
@@ -8,16 +8,30 @@ class VouchersController < ApplicationController
 
   def index
     @vouchers =  Controllers::FilterService.new(@vouchers,FIELD_FILTER_WHITELIST, filter_params).filter! if filter_params
-
-
   end
 
   def show
     @voucher
   end
 
+  def new
+    @voucher = Voucher.new    
+  end
+
+  def create
+   @voucher = Voucher.new(voucher_params)
+    if @voucher.save
+      redirect_to voucher_path(@voucher)
+    else
+      render :new
+    end
+  end
+
   private
 
+  def voucher_params
+    params.require(:voucher).permit()
+  end
   
   def filter_params
     params[:filters]&.compact_blank
