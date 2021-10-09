b1ef8c9b58452d04dd469acdda8c1c5b6d349910
Tom Ecrepont <tomecrepont@gmail.com>
2021-10-09 09:45:30 +0200

    Run rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_28_100903) do
+ActiveRecord::Schema.define(version: 2021_10_09_073624) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -109,7 +109,7 @@ ActiveRecord::Schema.define(version: 2021_09_28_100903) do
     t.string "full_address"
     t.string "zip_code"
     t.string "city"
-    t.string "gender"
+    t.integer "gender", default: 0, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
