1324cc178478f578db956fe4c443adb5a0ff184c
Tom Ecrepont <tomecrepont@gmail.com>
2021-10-12 18:24:23 +0200

    Change gender into an enum (#38)
    * Remove the old gender column
    
    * Create a new gender integer column and arbitrary set the gender to male or female depending on the id
    
    * Declare the enum into the user model
    
    * Run rails db:migrate
    
    * Setup pre commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :receipts
-
+  enum gender: [ :undefined, :male, :female ]
   def age
     Date.current.year - birthday.year
   end
